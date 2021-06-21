7daa3dbcced8797cff8f2c81d1ed680a6cfc6c39
Samuel Lord <sllord@icloud.com>
2021-06-20 20:53:34 -0500

    Update pages.js

--- pages.js
+++ pages.js
@@ -28,7 +28,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 HTMLArray = []
 
-HTMLArray.push(`<img src="me.png" width="5%"></img><h2>Hello, world! Welcome to my website.</h2><p>Welcome to sparkSammy.com! My name is Rusty, but you can just call me Sparksammy. However, please spell the brand "sparkSammy" as sparkSammy. My skills are programming, eating, sleeping, working with computer-based technology. If you see any silly posts from me on social media, they are probably from a long time ago or itâ€™s someone pretending to be me, so keep that in mind. Most importantly, enjoy my website!
+HTMLArray.push(`<img src="me.png" width="120vw"></img><h2>Hello, world! Welcome to my website.</h2><p>Welcome to sparkSammy.com! My name is Rusty, but you can just call me Sparksammy. However, please spell the brand "sparkSammy" as sparkSammy. My skills are programming, eating, sleeping, working with computer-based technology. If you see any silly posts from me on social media, they are probably from a long time ago or itâ€™s someone pretending to be me, so keep that in mind. Most importantly, enjoy my website!
 
 <br>
 <p><h3><i>Have a great one!</i></h3></p><p><h4><i>Peace!</i></h4></p><br><p align="center">"Be yourself; Everyone else is already taken".</p>
